145d369275d76991433536e4d02c767b579b0565
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2024-03-13 12:16:52 -0700

    Update Kokkos_Nul_Task.hpp: nul task

--- core/src/Nullified/Kokkos_Nul_Task.hpp
+++ core/src/Nullified/Kokkos_Nul_Task.hpp
@@ -89,7 +89,7 @@ template <class Scheduler>
 class TaskQueueSpecializationConstrained<
     Scheduler,
     std::enable_if_t<std::is_same<typename Scheduler::execution_space,
-                                  Kokkos::Serial>::value>> {
+                                  Kokkos::Nul>::value>> {
  public:
   // Note: Scheduler may be an incomplete type at class scope (but not inside
   // of the methods, obviously)
